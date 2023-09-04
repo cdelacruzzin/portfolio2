dfe4e4f16861db84b4ce2f529902ff6eff05868d
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-04 17:32:41 -0400

    useStates for storing input from form

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -5,28 +5,30 @@ import { checkPassword, validateEmail } from '../utils/helpers';
 export default function Contact() {
 
 
-  const [companyName, setCompanyName]= useState('');
-  const [name, setName]= useState('');
-  const [email, setEmail]= useState('');
-  const [inquiry, setInquiry]= useState('');
-  
-  const handleInputChange = (e)=>{
-    const target = e.target;
+  const [companyName, setCompanyName] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [inquiry, setInquiry] = useState('');
 
-    console.log(target);
-  }
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+    console.log(target)
+
+    if (inputType === 'company name') {
+      setCompanyName(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue);
+    } else {
+      setInquiry(inputValue);
+    }
+  };
 
 
-
-
-
-
-
-
-
-
-
-
+  console.log(inquiry);
 
   return (
     <div className=''>
@@ -36,16 +38,40 @@ export default function Contact() {
 
         <form className='d-flex flex-column gap-5' id='contact-form'>
           <label>Company name </label>
-          <input></input>
+          <input
+            value={companyName}
+            name="company name"
+            onChange={handleInputChange}
+            type="company name"
+            placeholder="company name"
+          />
 
           <label>Name <span>Required</span></label>
-          <input></input>
+          <input
+            value={name}
+            name='name'
+            onChange={handleInputChange}
+            type='name'
+            placeholder='name'
+          ></input>
 
           <label>Email <span>Required</span></label>
-          <input></input>
+          <input
+            value={email}
+            name='email'
+            type='email'
+            onChange={handleInputChange}
+            placeholder='email'
+          ></input>
 
           <label>Inquiry <span>Required</span></label>
-          <textArea></textArea>
+          <textarea
+            value={inquiry}
+            name='inquiry'
+            type='inquiry'
+            onChange={handleInputChange}
+            placeholder='Type here'
+          ></textarea>
 
           <div className='d-flex justify-content-center'>
             <button className='submit'>send</button>
